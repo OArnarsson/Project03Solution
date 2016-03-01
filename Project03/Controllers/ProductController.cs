750f4708d925806514fb73eda2bd94ba8e055a29
andriandresson <andriandresson@gmail.com>
2016-03-01 15:00:33 +0000

    asd

---
+++ Project03/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project03.Models;
+
+namespace Project03.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is the main page.";
+
+            Category category = new Category();
+            var model = category.GetAllProducts();
+            return View(model);
+        }
+
+
+    }
+}
